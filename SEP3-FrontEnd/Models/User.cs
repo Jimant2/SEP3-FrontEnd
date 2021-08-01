b1a25c5278196b2346b418809d41240571c51318
Jimant2 <jimantoniou2@gmail.com>
2021-08-01 12:25:29 +0200

    Added an account create page, will work on adding the majority of the other pages needed in this day

---
+++ SEP3-FrontEnd/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEP3_FrontEnd.Models
+
+{
+    public class User
+    {
+
+        public string username { get; set; }
+        public string password { get; set; }
+
+     
+    }
+}
